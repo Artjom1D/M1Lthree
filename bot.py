94f2f2f4760a366804d8a1d9d00695eec7ab8f5b
Loxnebot <loxnebot@gmail.com>
2025-08-13 21:21:35 +0300

    New command

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Привет! Я бот для управления чатом.")
+    bot.reply_to(message, "Привет! Я бот для управления чата.")
 
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
@@ -22,5 +22,21 @@ def ban_user(message):
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+
+@bot.message_handler(func=lambda message: "https://" in message.text or "http://" in message.text)
+def ban_link(message):
+    if not message.reply_to_message:
+        bot.reply_to(message, "Ссылки запрещены! Но команда должна быть ответом на сообщение пользователя.")
+        return
+    chat_id = message.chat.id
+    user_id = message.reply_to_message.from_user.id
+    user_status = bot.get_chat_member(chat_id, user_id).status
+
+    if user_status in ['administrator', 'creator']:
+        bot.reply_to(message, "Невозможно забанить администратора.")
+    else:
+        bot.ban_chat_member(chat_id, user_id)
+        bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен за ссылку.")
+
 
 bot.infinity_polling(none_stop=True)
